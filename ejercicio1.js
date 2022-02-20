b45ff1f5edec56c66f4184e665740be4b1b090d8
Lucia654 <lilian_jo2@hotmail.com>
2022-02-20 12:09:03 -0500

    ejercicio conteo letras

--- ejercicio1.js
+++
@@ -1,6 +0,0 @@
-let palabras = 'una palabra, dos palabras, tres palabras';
-
-for (let palabras = 0; palabras < 5; palabras++){
-    // se ejecuta el conteo
-    console.log('aumenta una palabra más')
-}
