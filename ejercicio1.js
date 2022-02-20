888e383964ba605f5747d40c20992a9ef5b043c9
Lucia654 <lilian_jo2@hotmail.com>
2022-02-20 10:00:07 -0500

    agrego ejercicio-prueba

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+let palabras = 'una palabra, dos palabras, tres palabras';
+
+for (let palabras = 0; palabras < 5; palabras++){
+    // se ejecuta el conteo
+    console.log('aumenta una palabra más')
+}
